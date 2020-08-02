63dd38c6d041374ce26ad5a4fc725f72beb9a918
BuildTools <kcai1@terpmail.umd.edu>
2020-08-01 22:41:41 -0400

    finished front end (swing timer)

--- SortingAlgoVisualizer/src/Display.java
+++ SortingAlgoVisualizer/src/Display.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 
 //All pixel lengths in this class are centered around working with 100 elements (11 pixel width per element) 
@@ -18,12 +19,19 @@ public class Display extends JPanel {
 	private static final int ORANGE = 1;
 	private static final int GREEN = 2;
 	
+	// Hacky solution to run repaint() properly
+	private static int index = 0;
+	private Timer genNewList;
+	private Random rand;
+	
 	private Elements elements;
 	private Elements.Node[] nodes;
 	private SortingAlgos sorter;
 	
 	public Display() {
 		setLayout(null);
+		rand = new Random();
+		
 		elements = new Elements();
 		nodes = elements.getElements();
 		sorter = new SortingAlgos(nodes);
@@ -31,8 +39,9 @@ public class Display extends JPanel {
 		JButton button1 = new JButton("Generate New List");
 		button1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				generateNewList();
+				genNewListHelper();
 				repaint(); // Calls printComponent()
+				check();
 			}
 		});
 		button1.setBounds(100, 600, 150, 30);
@@ -47,6 +56,28 @@ public class Display extends JPanel {
 		});
 		button2.setBounds(275, 600, 150, 30);
 		add(button2);
+		
+		// Shuffles the nodes/elements, pauses 1 ms per swap
+		// Nodes are simply re-ordered, no new Nodes are created
+		genNewList = new Timer(1, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(index >= elements.getCount()) {
+					genNewList.stop();
+					index = 0;
+					return;
+				} else {
+					Elements.Node temp = nodes[index];
+					int newIndex = rand.nextInt(elements.getCount());
+					nodes[index] = nodes[newIndex];
+					nodes[newIndex] = temp;
+					index++;
+					repaint();
+				}
+			}
+			
+		});
 	}
 	
 	private Color getColor(int code) {
@@ -61,6 +92,7 @@ public class Display extends JPanel {
 	}
 	
 	// The height, order, and state (color) of the lines are drawn based on the information stored in the nodes
+	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		for(int i = 0; i < elements.getCount(); i++) {
@@ -70,17 +102,20 @@ public class Display extends JPanel {
 		}
 	}
 	
-	// Shuffles the nodes/elements
-	// Nodes are simply re-ordered, no new Nodes are created
-	private void generateNewList() {
-		Random rand = new Random();
-		
-		for(int i = 0; i < elements.getCount(); i++) {
-			Elements.Node temp = nodes[i];
-			int newIndex = rand.nextInt(elements.getCount());
-			nodes[i] = nodes[newIndex];
-			nodes[newIndex] = temp;
-		}
+	private void genNewListHelper() {
+		genNewList.start();
 	}
 	
+	private void check() { // Total value should add up to 25750
+		int total = 0;
+		for(int i = 0; i < nodes.length; i++) {
+			total += nodes[i].getVal();
+		}
+		
+		if(total == 25750) {
+			System.out.println("Passes immutable node vals check");
+		} else {
+			System.out.println("FAILED Total vals was: " + total);
+		}
+	}
 }
