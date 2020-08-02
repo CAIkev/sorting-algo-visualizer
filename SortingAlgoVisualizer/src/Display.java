cc7435c9be7ba1f25804d2a092a3dc0844d12959
BuildTools <kcai1@terpmail.umd.edu>
2020-08-02 15:17:38 -0400

    bubble sort implementation

--- SortingAlgoVisualizer/src/Display.java
+++ SortingAlgoVisualizer/src/Display.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 
-//All pixel lengths in this class are centered around working with 100 elements (11 pixel width per element) 
+//All pixel lengths in this class are centered around working with 100 elements (10 pixel width per element) 
 public class Display extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
@@ -19,53 +19,35 @@ public class Display extends JPanel {
 	private static final int ORANGE = 1;
 	private static final int GREEN = 2;
 	
-	// Hacky solution to run repaint() properly
-	private static int index = 0;
-	private Timer genNewList;
-	private Random rand;
-	
 	private Elements elements;
 	private Elements.Node[] nodes;
 	private SortingAlgos sorter;
 	
+	private JButton[] buttons;
+	private Timer genNewList;
+	
 	public Display() {
 		setLayout(null);
-		rand = new Random();
-		
+		initClickables();
+		initListGenerator();
 		elements = new Elements();
 		nodes = elements.getElements();
-		sorter = new SortingAlgos(nodes);
-		
-		JButton button1 = new JButton("Generate New List");
-		button1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				genNewListHelper();
-				repaint(); // Calls printComponent()
-				check();
-			}
-		});
-		button1.setBounds(100, 600, 150, 30);
-		add(button1);
-		
-		JButton button2 = new JButton("Bubblesort");
-		button2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Run bubblesort (no functionality right now)");
-				repaint(); // Calls printComponent()
-			}
-		});
-		button2.setBounds(275, 600, 150, 30);
-		add(button2);
-		
-		// Shuffles the nodes/elements, pauses 1 ms per swap
-		// Nodes are simply re-ordered, no new Nodes are created
+		sorter = new SortingAlgos(this, nodes);
+	}
+	
+	// Shuffles the nodes/elements, pauses 1 ms per swap
+	// Nodes are simply re-ordered, no new Nodes are created
+	private void initListGenerator() {
 		genNewList = new Timer(1, new ActionListener() {
-
+			private int index = 0;
+			Random rand = new Random();
+			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				if(index >= elements.getCount()) {
 					genNewList.stop();
 					index = 0;
+					disableButtons(false);
 					return;
 				} else {
 					Elements.Node temp = nodes[index];
@@ -78,6 +60,44 @@ public class Display extends JPanel {
 			}
 			
 		});
+	}
+	
+	public void disableButtons(Boolean disable) {
+		if(disable) {
+			for(JButton x : buttons) {
+				x.setEnabled(false);
+			}
+		} else {
+			for(JButton x : buttons) {
+				x.setEnabled(true);
+			}
+		}
+	}
+	
+	private void initClickables() {
+		buttons = new JButton[2];
+		
+		JButton button1 = new JButton("Generate New List");
+		button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				disableButtons(true);
+				genNewList.start();
+			}
+		});
+		button1.setBounds(100, 600, 150, 30);
+		add(button1);
+		buttons[0] = button1;
+		
+		JButton button2 = new JButton("Bubblesort");
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				disableButtons(true);
+				sorter.runBubbleSort();
+			}
+		});
+		button2.setBounds(275, 600, 150, 30);
+		add(button2);
+		buttons[1] = button2;
 	}
 	
 	private Color getColor(int code) {
@@ -97,15 +117,12 @@ public class Display extends JPanel {
 		super.paintComponent(g);
 		for(int i = 0; i < elements.getCount(); i++) {
 			g.setColor(getColor(nodes[i].getCode()));
-			nodes[i].setCode(BLACK);
+			nodes[i].setCode(BLACK); // Reset color to black
 			g.fillRect(87 + (i*11), 0, 10, nodes[i].getVal());
 		}
 	}
 	
-	private void genNewListHelper() {
-		genNewList.start();
-	}
-	
+	/*
 	private void check() { // Total value should add up to 25750
 		int total = 0;
 		for(int i = 0; i < nodes.length; i++) {
@@ -117,5 +134,5 @@ public class Display extends JPanel {
 		} else {
 			System.out.println("FAILED Total vals was: " + total);
 		}
-	}
+	}*/
 }
--- SortingAlgoVisualizer/src/SortingAlgos.java
+++ SortingAlgoVisualizer/src/SortingAlgos.java
@@ -1,15 +1,74 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
 
 public class SortingAlgos {
 
 	// Corresponds to codes in Display class
-	private static final int IGNORE = 0;
+	// private static final int RESET = 0; // Display resets code to 0 
 	private static final int COMPARE = 1;
 	private static final int SWAP = 2;
 	
+	private Display dis;
 	private Elements.Node[] nodes;
 	
-	public SortingAlgos(Elements.Node[] list) {
+	private Timer bubbleSort;
+	
+	public SortingAlgos(Display disIn, Elements.Node[] list) {
+		dis = disIn;
 		nodes = list;
+	}
+	
+	private void initBubbleSort() {
+		bubbleSort = new Timer(1, new ActionListener() {
+			private int i = 0;
+			private int j = 0;
+			private Elements.Node temp;
+			private Boolean forcedChange = false;
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(i >= nodes.length-1) {
+					bubbleSort.stop();
+					dis.repaint();
+					dis.disableButtons(false);
+					return;
+				}
+				if(j >= nodes.length-i-1) {
+					j = 0;
+					i++;
+					return;
+				}
+				
+				if(!forcedChange && nodes[j].getVal() <= nodes[j+1].getVal()) {
+					forcedChange = true;
+					nodes[j].setCode(COMPARE);
+					nodes[j+1].setCode(COMPARE);
+					dis.repaint();
+					return;
+				}
+				if(forcedChange) {
+					forcedChange = false;
+				}
+				
+				if(nodes[j].getVal() > nodes[j+1].getVal()) {
+					temp = nodes[j];
+					nodes[j] = nodes[j+1];
+					nodes[j+1] = temp;
+					nodes[j].setCode(SWAP);
+					nodes[j+1].setCode(SWAP);
+				}
+				dis.repaint();
+				j++;
+			}
+			
+		});
+	}
+
+	public void runBubbleSort() {
+		initBubbleSort();
+		bubbleSort.start();
 	}
 	
 }
